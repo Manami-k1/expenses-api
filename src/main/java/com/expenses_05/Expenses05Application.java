0312f4ef37104aaf809871d0721476519783b661
Kagaya Manami <ys.traning230715@gmail.com>
2025-06-08 21:37:27 +0900

    first commit

---
+++ src/main/java/com/expenses_05/Expenses05Application.java
@@ -0,0 +1,13 @@
+package com.expenses_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expenses05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Expenses05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenses_05/Expenses05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Expenses05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
